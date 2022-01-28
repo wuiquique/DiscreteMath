506e552e9a66f89c8095f631613f4fadb6a15631
wuiquique <luisenriquem15@gmail.com>
2022-01-28 01:56:15 +0000

    Evaluacion 1 final

--- Cifrado/README.md
+++ Cifrado/README.md
@@ -2,3 +2,5 @@ Evaluacion 1
 
 Se hizo un programa de C++ para poder cifrar un mensaje segun lo pedido en las instruccion. 
 Para compilar se utilizo g++. El comando "g++ main.cc" nos compila el codigo en el archivo llamado "a.out".
+
+Se utilizaron vectores para la realizacion de esta evaluacion. Haciendo todas las multiplicaciones con un modulo 27, aun que en el listado no se agrego la ñ se coloca el modulo mencionado para que los resultados fueran iguales a los vistos en clase. 
--- Cifrado/main.cc
+++ Cifrado/main.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
 int main() {
     
     string text;
+    int mod;
+    vector<int> res;
+    vector<int> num;
+    vector<char> cifrado;
     vector<char> vocab = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     cout << "Escriba el mensaje a cifrar: ";
@@ -13,7 +18,9 @@ int main() {
     cout << endl;
     transform(text.begin(), text.end(), text.begin(), ::toupper);
 
-    cout << "El mensaje cifrado es: ";
+    cout << "Escriba el modulo para cifrar (int): ";
+    cin >> mod;
+    cout << endl;
 
     for(int i = 0; i < text.size(); i++) {
 
@@ -21,9 +28,26 @@ int main() {
 
             if(text[i] == vocab[j]){
 
-                cout << j;
+                res.push_back(j);
             }
         }
+    }
+
+    for(int i = 0; i < res.size(); i++){
+
+        num.push_back( (mod*res[i]) % 27);
+    }
+
+    for(int i = 0; i < num.size(); i++) {
+
+        cifrado.push_back(vocab[num[i]]);
+    }
+
+    cout << "EL mensaje cifrado es: ";
+
+    for(int i = 0; i < cifrado.size(); i++) {
+
+        cout << cifrado[i];
     }
 
     cout << endl;
